dd02f40de46f552ad378000a9ffb1196bc49d164
PC <lakshmdu84@gmail.com>
2024-07-20 18:44:53 +0530

    changed logic

--- src/main/java/DuplicateChar.java
+++ src/main/java/DuplicateChar.java
@@ -7,7 +7,7 @@ public class DuplicateChar {
         char data[] = val.toCharArray();
         for(int i=0;i<data.length-1;i++) {
             if(data[i]==data[i+1]) {
-                System.out.println(data[i+1]+" "+i+1);
+                System.out.println(data[i+1]+" "+(i+1));
                 break;
             }
         }
